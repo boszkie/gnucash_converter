02a4e5d79023b48f5e9316deedc9a8b64c98406d
Erik de Bos <erik@debos-webdev.nl>
2018-12-29 14:44:12 +0100

    fix test after rename, remove double self

--- tests/test_gnuCashConverter.py
+++ tests/test_gnuCashConverter.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from GnuCashConverter import *
 
+
 class TestGnuCashConverter(TestCase):
     '''
     test that we consistently import a single csv row
@@ -12,15 +13,15 @@ class TestGnuCashConverter(TestCase):
             row = self.extractRow(openCsvFile)
 
             self.assertEqual(row['date'], '2017-12-25')
-            self.assertEqual(row['withdrawal'], 0)
-            self.assertEqual(row['deposit'], Decimal('1800.00'))
+            self.assertEqual(row['deposit'], 0)
+            self.assertEqual(row['withdrawal'], Decimal('1800.00'))
             self.assertEqual(row['balance'], Decimal('6312.58'))
             self.assertEqual(row['message'], 'NL54RABO0143316680 RABONL2UXXX E. DE BOS EO voor de huisrekening en hypotheek bg')
 
     '''
     extract rows from csv
     '''
-    def extractRow(selfself, csvFile):
+    def extractRow(self, csvFile):
         converter = rabobankConverter(csv.reader(csvFile, delimiter=',', quotechar='"'))
 
         converter.setInitialBalance(123)
